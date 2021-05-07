1745ea37c9eefb01f259cc6f702e44ed3b0ff856
unknown <saki.maruyama@pasnet.local>
2021-05-07 09:53:39 +0900

    0507 0953 commit

--- DSStuck001.php
+++ DSStuck001.php
@@ -23,9 +23,10 @@ class Stack {
     }
 
     //get top
-    public function top(){
+    public function top()
+    {
         if (!$this->isEmpty()){
-            return $this->elements[sizeof($this->elements) -1];
+            return $this->elements[sizeof($this->elements) - 1];
         }
         return null;
     }
@@ -34,5 +35,36 @@ class Stack {
     public function isEmpty(){
         return empty($this->elements);
     }
+
+    //elementsはprivateなのでクラス内でしか処理ができない
+    //なので要素数を調べる関数を追加しておく
+    public function size(){
+        return sizeof($this->elements);
+    }
 }
 
+function removeCoupleOfWords($words){
+    $stack = new Stack();
+
+    for ($i = 0; $i < count($words); $i++){
+        //空だったら単純にpushする
+        if ($stack->isEmpty()){
+            $stack->push($words[$i]);
+        } else {
+            //今のスタックのtopの値を取得
+            $str = $stack->top();
+            
+            //入れようとしている値とtopの値が同じならpop削除
+            if ($words[$i] == $str) {
+                $stack->pop();
+            } else { //入れようとしている値とtopの値が違うのでpushで値を入れる
+                $stack->push($words[$i]);
+            }
+        }
+    }
+
+    return $stack->size();
+}
+
+$words = ["ab", "aa", "aa", "bcd", "ab"];
+echo removeCoupleOfWords($words);
